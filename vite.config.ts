028084bb32e484d571968b8077fab5d4b33f1394
Jespermv <jesper.vaisanen@metropolia.fi>
2025-01-23 11:16:40 +0200

    test examples

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
+/// <reference types="vitest/config" />
+// https://vitest.dev/config/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/~jespermv/Hybridisovellukset/Week2-3/',
-  //base: '',
+  // Käytä users.metropolia basea vain jos ajat users.metropoliassa. Muuten käytä basea ''.
+  //base: '/~jespermv/Hybridisovellukset/Week2-3/',
+  base: '',
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './vitest-setup.js',
+  },
 })
+
